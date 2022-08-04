f37f4ed8a0fc224e85c3697cab1a8c291c73f932
Lucas <lucasmsaldana@gmail.com>
2022-08-04 17:27:54 -0300

    Se finaliza la pantalla de Login

---
+++ src/sistemaventas/SistemaVentas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemaventas;
+import Pantalla.Login;
+import Pantalla.*;
+
+
+/**
+ *
+ * @author Lucas
+ */
+public class SistemaVentas {
+    public static void main(String[] args) {
+     Login inicio=new Login();
+     inicio.setLocationRelativeTo(null);
+     inicio.setVisible(true);
+     //Principal main= new Principal();
+     //main.setLocationRelativeTo(null);
+     //main.setVisible(true);
+      
+      
+    }
+    
+}
