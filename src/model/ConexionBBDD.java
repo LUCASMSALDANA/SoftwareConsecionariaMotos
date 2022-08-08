4f0ccf7ead70f1710363ae475e1e5d379f6e55b5
Lucas <lucasmsaldana@gmail.com>
2022-08-08 17:10:57 -0300

    Finalizacion Pantalla de Nueva Venta

---
+++ src/model/ConexionBBDD.java
@@ -0,0 +1,23 @@
+
+package model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ConexionBBDD {
+    Connection con;
+    public Connection getConnection(){
+        try{
+            String BBDD = "jdbc:mysql://localhost:3306/consecionaria?serverTimezone=UTC";
+            con = DriverManager.getConnection(BBDD,"root","root");
+            return con;
+         }catch(SQLException e){
+             System.out.println(e.toString());
+         }
+         return null;
+    }
+}
