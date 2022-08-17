a686cfe3c7da1da1c5acce85dd3536e6439250db
Lucas <lucasmsaldana@gmail.com>
2022-08-17 11:05:44 -0300

    Sacamos algunas líneas de más

--- src/Dao/UsuarioDao.java
+++ src/Dao/UsuarioDao.java
@@ -5,9 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import model.cliente.Cliente;
+
 
 /**
  *
